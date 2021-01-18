4dcd60a025cb68a5890e079f29b504e654ce95e7
C0118185 <65756852+C0118185@users.noreply.github.com>
2021-01-18 18:19:04 +0900

    Update BLE.py

--- BLE.py
+++ BLE.py
@@ -1,3 +1,30 @@
-class ubluetooth.BLE
-BLE.active([active, ]/)
-BLE.gap_advertise(interval_us, adv_data=None, *, resp_data=None, connectable=True)
+%serialconnect to --port=/dev/ttyUSB0 --baud=115200
+
+import ubluetooth
+import utime
+from micropython import const
+
+_IRQ_CENTRAL_CONNECT                 = const(1 << 0)
+_IRQ_CENTRAL_DISCONNECT              = const(1 << 1)
+_IRQ_GATTS_WRITE                     = const(1 << 2)
+_IRQ_GATTS_READ_REQUEST              = const(1 << 3)
+_IRQ_SCAN_RESULT                     = const(1 << 4)
+_IRQ_SCAN_COMPLETE                   = const(1 << 5)
+_IRQ_PERIPHERAL_CONNECT              = const(1 << 6)
+_IRQ_PERIPHERAL_DISCONNECT           = const(1 << 7)
+_IRQ_GATTC_SERVICE_RESULT            = const(1 << 8)
+_IRQ_GATTC_CHARACTERISTIC_RESULT     = const(1 << 9)
+_IRQ_GATTC_DESCRIPTOR_RESULT         = const(1 << 10)
+_IRQ_GATTC_READ_RESULT               = const(1 << 11)
+_IRQ_GATTC_WRITE_STATUS              = const(1 << 12)
+_IRQ_GATTC_NOTIFY                    = const(1 << 13)
+_IRQ_GATTC_INDICATE                  = const(1 << 14)
+
+ble=ubluetooth.BLE()
+
+
+if ble.active()== False:
+    ble.active(True)
+
+    print(ble.active())
+    print('BLE current mac address ', ble.config('mac'))
