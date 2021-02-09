628b1ca2a27bf0e1b7181c0d390ecd35986985dc
C0118185 <65756852+C0118185@users.noreply.github.com>
2021-02-09 11:09:35 +0900

    Update bmp280show.py

--- bmp280show.py
+++ bmp280show.py
@@ -1,8 +1,8 @@
-from machine import I2C
+from machine import I2C, Pin
 from bmp280 import BMP280
  
 bus = I2C(sda=Pin(21),scl=Pin(22))
 bmp = BMP280(bus)
- 
+
 print(bmp.temperature)
 print(bmp.pressure)
