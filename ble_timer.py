20f6de7ce3359af32b88d6ec9c2a262c8020e835
C0118185 <65756852+C0118185@users.noreply.github.com>
2021-02-17 16:54:45 +0900

    Add files via upload

---
+++ ble_timer.py
@@ -0,0 +1,121 @@
+# This example demonstrates a simple temperature sensor peripheral.
+#
+# The sensor's local value updates every second, and it will notify
+# any connected central every 10 seconds.
+
+import bluetooth
+import random
+import struct
+import time
+import timer
+from ble_advertising import advertising_payload
+
+from micropython import const
+
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_INDICATE_DONE = const(20)
+
+_FLAG_READ = const(0x0002)
+_FLAG_NOTIFY = const(0x0010)
+_FLAG_INDICATE = const(0x0020)
+
+# org.bluetooth.service.environmental_sensing
+_ENV_SENSE_UUID = bluetooth.UUID(0x181A)
+# org.bluetooth.characteristic.temperature
+_TEMP_CHAR = (
+    bluetooth.UUID(0x2A6E),
+    _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,
+)
+_ENV_SENSE_SERVICE = (
+    _ENV_SENSE_UUID,
+    (_TEMP_CHAR,),
+)
+
+# org.bluetooth.characteristic.gap.appearance.xml
+_ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
+
+
+class BLETemperature:
+    def __init__(self, ble, name="esp32-chaiA"):
+        self._ble = ble
+        self._ble.active(True)
+        self._ble.irq(self._irq)
+        ((self._handle,),) = self._ble.gatts_register_services((_ENV_SENSE_SERVICE,))
+        self._connections = set()
+        self._payload = advertising_payload(
+            name=name, services=[_ENV_SENSE_UUID], appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER
+        )
+        self._advertise()
+
+    def _irq(self, event, data):
+        # Track connections so we can send notifications.
+        if event == _IRQ_CENTRAL_CONNECT:
+            conn_handle, _, _ = data
+            self._connections.add(conn_handle)
+        elif event == _IRQ_CENTRAL_DISCONNECT:
+            conn_handle, _, _ = data
+            self._connections.remove(conn_handle)
+            # Start advertising again to allow a new connection.
+            self._advertise()
+        elif event == _IRQ_GATTS_INDICATE_DONE:
+            conn_handle, value_handle, status = data
+
+    def set_temperature(self, time_deg_c, notify=False, indicate=False):
+        # Data is sint16 in degrees Celsius with a resolution of 0.01 degrees Celsius.
+        # Write the local value, ready for a central to read.
+        self._ble.gatts_write(self._handle, struct.pack("<q", int(time_deg_c)))
+        if notify or indicate:
+            for conn_handle in self._connections:
+                if notify:
+                    # Notify connected centrals.
+                    self._ble.gatts_notify(conn_handle, self._handle)
+                if indicate:
+                    # Indicate connected centrals.
+                    self._ble.gatts_indicate(conn_handle, self._handle)
+
+    def _advertise(self, interval_us=500000):
+        self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
+
+def demo():
+    ble = bluetooth.BLE()
+    temp = BLETemperature(ble)
+
+    t = 25
+    i = 0
+    
+    count = 0
+
+    # 二回データを出す 
+    #while count < 2:
+        # Write every second, notify every 10 seconds.
+        #i = (i + 1) % 10
+        #temp.set_temperature(t, notify=i == 0, indicate=False)
+        # Random walk the temperature.
+        #t += random.uniform(-0.5, 0.5)
+        #print(t)
+        #time.sleep_ms(1000)
+        #count += 1
+
+    # 無限ループのオプション
+    #while True:
+        # Write every second, notify every 10 seconds.
+        #i = (i + 1) % 10
+        #temp.set_temperature(t, notify=i == 0, indicate=False)
+        # Random walk the temperature.
+        #t += random.uniform(-0.5, 0.5)
+        #print(t)
+        #time.sleep_ms(1000)
+
+    # 時間評価のオプション
+    while True:
+        ti = timer.get_time()
+        temp.set_temperature(ti, notify=i == 0, indicate=False)
+        print(ti)
+        time.sleep_ms(1000)
+    
+
+
+if __name__ == "__main__":
+    demo()
